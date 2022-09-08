b7d8a11abb78e234d1454e10655691d22dd188fb
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2022-09-08 18:49:28 +0800

    update hash

--- foaks/infrastructure/src/my_hash.rs
+++ foaks/infrastructure/src/my_hash.rs
@@ -1,4 +1,19 @@
+use sha3::{Digest, Sha3_256};
+
+#[derive(Clone, Copy, Default, PartialEq, Eq)]
 pub struct HashDigest {
-    h0: i128,
-    h1: i128,
+    pub h0: i128,
+    pub h1: i128,
+}
+
+// NOTE: no need to implement
+// #[inline]
+// pub fn equals(a: &HashDigest, b: &HashDigest) -> bool {
+
+// }
+
+// TODO: implement
+#[inline]
+pub fn my_hash(src: [HashDigest; 2], dst: &mut HashDigest) {
+
 }
