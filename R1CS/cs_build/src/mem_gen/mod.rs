7ddcba9bd06f07c88679dd304b3919f5eecd6069
zengzengzenghuy <tayzh2799@gmail.com>
2022-09-27 17:30:18 +0800

    WIP: added val[0..7] check in matrix

--- R1CS/cs_build/src/mem_gen/mod.rs
+++ R1CS/cs_build/src/mem_gen/mod.rs
@@ -33,7 +33,7 @@ pub fn boolean_check_matrix_gen(x_in: u64) -> ConstraintMatrices<Fr> {
     cs.finalize();
     assert!(cs.is_satisfied().is_ok());
     let matrices = cs.to_matrices().unwrap();
-
+    // one, out, x
     // A [0,0,1]
     // B [-1,0,1]
     // C [0,1,0]
@@ -102,9 +102,11 @@ pub fn mOp_mWr_check_matrix_gen(mOp_in: u64, mWr_in: u64) -> ConstraintMatrices<
 }
 
 // Constraint: (1-mOp'*mWr')(1-lastAccess)(val[0..7]'-val[0..7])=0
+// Constraint: (1-mOp'*mWr')(1-lastAccess)(val'[0..3]-val[0..3])(val'[4..7]-val[4..7])=0
 // mid_1 = mOp'*mWr'
 // mid_2 = (1-mid_1)(1-lastAccess)
-// out = mid_2*(val[0..7]'-val[0..7])
+// mid_3 = mid_2*(val'[0..3]-val[0..3])
+// out = mid_3*(val'[4..7]-val[4..7])
 pub fn update_value_check_matrix_gen(
     mOp_in: u64,
     mWr_in: u64,
@@ -113,10 +115,18 @@ pub fn update_value_check_matrix_gen(
     val_p1: u64,
     val_p2: u64,
     val_p3: u64,
+    val_p4: u64,
+    val_p5: u64,
+    val_p6: u64,
+    val_p7: u64,
     val_0: u64,
     val_1: u64,
     val_2: u64,
     val_3: u64,
+    val_4: u64,
+    val_5: u64,
+    val_6: u64,
+    val_7: u64,
 ) -> ConstraintMatrices<Fr> {
     let cs = ConstraintSystem::<Fr>::new_ref();
     let mOp = Fr::from(mOp_in);
@@ -129,24 +139,40 @@ pub fn update_value_check_matrix_gen(
     let val_p1: u128 = val_p1 as u128;
     let val_p2: u128 = val_p2 as u128;
     let val_p3: u128 = val_p3 as u128;
+    let val_p4: u128 = val_p0 as u128;
+    let val_p5: u128 = val_p1 as u128;
+    let val_p6: u128 = val_p2 as u128;
+    let val_p7: u128 = val_p3 as u128;
     let val_0: u128 = val_0 as u128;
     let val_1: u128 = val_1 as u128;
     let val_2: u128 = val_2 as u128;
     let val_3: u128 = val_3 as u128;
-    let val_p: u128 = val_p0 << 96 + val_p1 << 64 + val_p2 << 32 + val_p3;
-    let val_p = Fr::from(val_p);
-    let val: u128 = val_0 << 96 + val_1 << 64 + val_2 << 32 + val_3;
-    let val = Fr::from(val);
+    let val_4: u128 = val_0 as u128;
+    let val_5: u128 = val_1 as u128;
+    let val_6: u128 = val_2 as u128;
+    let val_7: u128 = val_3 as u128;
+    let val_p1: u128 = val_p0 << 96 + val_p1 << 64 + val_p2 << 32 + val_p3;
+    let val_p1 = Fr::from(val_p1);
+    let val_1: u128 = val_0 << 96 + val_1 << 64 + val_2 << 32 + val_3;
+    let val_1 = Fr::from(val_1);
+    let val_p2: u128 = val_p4 << 96 + val_p5 << 64 + val_p6 << 32 + val_p7;
+    let val_p2 = Fr::from(val_p2);
+    let val_2: u128 = val_4 << 96 + val_5 << 64 + val_6 << 32 + val_7;
+    let val_2 = Fr::from(val_2);
     let mid_2 = Fr::from((one - mid1) * (one - lastAccess)); // all Fr::from is of type Fp256<FrParameters>
+    let mid_3 = Fr::from(mid_2 * (val_p2 - val_2));
 
     let mid_1 = cs.new_witness_variable(|| Ok(mOp * mWr)).unwrap(); // all  variable is type r1cs::Variable
 
     let mid_2 = cs.new_witness_variable(|| Ok(mid_2)).unwrap();
+    let mid_3 = cs.new_witness_variable(|| Ok(mid_3)).unwrap();
     let mOp = cs.new_witness_variable(|| Ok(mOp)).unwrap();
     let mWr = cs.new_witness_variable(|| Ok(mWr)).unwrap();
     let lastAccess = cs.new_witness_variable(|| Ok(lastAccess)).unwrap();
-    let val_p = cs.new_witness_variable(|| Ok(val_p)).unwrap();
-    let val = cs.new_witness_variable(|| Ok(val)).unwrap();
+    let val_p1 = cs.new_witness_variable(|| Ok(val_p1)).unwrap();
+    let val_1 = cs.new_witness_variable(|| Ok(val_1)).unwrap();
+    let val_p2 = cs.new_witness_variable(|| Ok(val_p2)).unwrap();
+    let val_2 = cs.new_witness_variable(|| Ok(val_2)).unwrap();
 
     let out = cs.new_input_variable(|| Ok(Fr::from(0u128))).unwrap();
 
@@ -158,22 +184,27 @@ pub fn update_value_check_matrix_gen(
         lc!() + mid_2,
     )
     .unwrap();
-    cs.enforce_constraint(lc!() + mid_2, lc!() + val_p - val, lc!() + out)
+    cs.enforce_constraint(lc!() + mid_2, lc!() + val_p1 - val_1, lc!() + mid_3)
+        .unwrap();
+    cs.enforce_constraint(lc!() + mid_3, lc!() + val_p2 - val_2, lc!() + out)
         .unwrap();
     cs.finalize();
 
     assert!(cs.is_satisfied().is_ok());
     let matrices = cs.to_matrices().unwrap();
-    // 1, out, mid_1, mid_2, mOpr, mWr, lastAccess, val_p, val
-    // A [0, 0, 0, 0, 1, 0, 0, 0]
-    //   [1, 0, -1, 0, 1, 0, 0, 0, 0]
-    //   [0, 0, 0, 1, 0, 0, 0, 0, 0]
-    // B [0, 0, 0, 0, 0, 1, 0, 0, 0]
-    //   [1, 0, 0, 0, 0, 0, -1, 0, 0]
-    //   [0, 0, 0, 0, 0, 0, 0, 1, -1]
-    // C [0, 0, 1, 0, 0, 0, 0, 0, 0]
-    //   [0, 0, 0, 1, 0, 0, 0, 0, 0]
-    //   [0, 1, 0, 0, 0, 0, 0, 0, 0]
+    // 1, out, mid_1, mid_2, mid_3, mOpr, mWr, lastAccess, val_p1, val_1, val_p2, val_2
+    // A [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    //   [1, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    //   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    //   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    // B [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    //   [1, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0]
+    //   [0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0]
+    //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1]
+    // C [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    //   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    //   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    //   [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     assert_eq!(matrices.a[0], vec![(Fr::from(1u64), 4)]);
     assert_eq!(matrices.b[0], vec![(Fr::from(1u64), 5)]);
