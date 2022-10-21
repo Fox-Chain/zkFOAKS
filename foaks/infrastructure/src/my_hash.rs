654ae591a5f00971376139516cff0da2a424ea30
zypeh <zypeh@users.noreply.github.com>
2022-10-21 20:41:57 +0800

    Some byte marshalling

--- foaks/infrastructure/src/my_hash.rs
+++ foaks/infrastructure/src/my_hash.rs
@@ -1,23 +1,41 @@
+use byteorder::{BigEndian, ReadBytesExt};
 use sha3::{Digest, Sha3_256};
+use std::io::Cursor;
 
-use crypto::sha2::sha256_digest_block;
-
+/// TODO: https://doc.rust-lang.org/beta/core/arch/x86_64/struct.__m128i.html
 #[derive(Clone, Copy, Default, PartialEq, Eq)]
 pub struct HashDigest {
     pub h0: i128,
     pub h1: i128,
 }
 
-// NOTE: no need to implement
-// #[inline]
-// pub fn equals(a: &HashDigest, b: &HashDigest) -> bool {
+impl HashDigest {
+    pub fn new() -> Self {
+        HashDigest { h0: 0, h1: 0 }
+    }
+}
 
-// }
-
-// TODO: implement
 #[inline]
-pub fn my_hash(src: [HashDigest; 2], dst: &mut HashDigest) {
-    // the original sha256_update_shani type signature is
-    // static void update_shani(uint32_t *state, const uint8_t *msg, uint32_t num_blocks)
-    // return sha256_digest_block(src, dst);
+pub fn my_hash(src: [HashDigest; 2]) -> HashDigest {
+    // the original sha256_update_shani type signature is an optimised function for
+    // SHA-NI instruction sets machine, this is the fallback one
+    // https://www.ic.unicamp.br/~ra142685/sok-apkc.pdf
+    let mut hasher = Sha3_256::new();
+
+    for d in src {
+        let sixteen_u8_h = d.h0.to_be_bytes();
+        let sixteen_u8_l = d.h1.to_be_bytes();
+        hasher.update(sixteen_u8_h);
+        hasher.update(sixteen_u8_l);
+    }
+
+    let result = hasher.finalize();
+
+    // DO WE REALLY SURE SHA256 produces fixed-length output?
+    let (arr, _) = result.as_chunks::<{ 16 as usize }>();
+
+    HashDigest {
+        h0: Cursor::new(arr[0]).read_i128::<BigEndian>().unwrap(),
+        h1: Cursor::new(arr[1]).read_i128::<BigEndian>().unwrap(),
+    }
 }
