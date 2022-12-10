c5727c7de2c8ea6fd25c824006aebdcd9c0c6c1d
zypeh <zypeh@users.noreply.github.com>
2022-12-11 01:54:11 +0800

    request_init_value_with_merkle done

--- foaks/vpd/src/verifier.rs
+++ foaks/vpd/src/verifier.rs
@@ -43,7 +43,7 @@ pub fn verify_merkle(
         let data_element: [FieldElement; 2] = [value.0, value.1];
         // can't coerce type FieldElement to HashDigest
         // data_element[1] = value_hash; // ?
-        value_hash = my_hash::my_hash(data_element);
+        // value_hash = my_hash::my_hash(data_element);
     }
 
     // QUESTION: should check if the function equals is custom one?
@@ -62,11 +62,104 @@ pub struct FriCommitPhaseData {
     pub rs_codeword_mapping: Vec<[usize; MAX_FRI_DEPTH]>,
 }
 
+impl FRIContext {
+    /// Request two values w^{pow0} and w^{pow1}, with merkle tree proof, where w is the root of unity and w^{pow0} and w^{pow1} are quad residue. Repeat ldt_repeat_num times, storing all result in vector.
+    pub fn request_init_value_with_merkle(
+        &mut self,
+        pow_0: usize,
+        pow_1: usize,
+        // new_size: &i64,
+        oracle_indicator: usize,
+    ) -> (Vec<(FieldElement, FieldElement)>, Vec<HashDigest>) {
+        // we swap pow_0 and pow_1 when pow_0 > pow_1
+        let (pow_0, pow_1) = if pow_0 > pow_1 {
+            (pow_1, pow_0)
+        } else {
+            (pow_0, pow_1)
+        };
+
+        assert!(pow_0 + (1 << self.log_current_witness_size_per_slice) / 2 == pow_1);
+
+        let mut value: Vec<(FieldElement, FieldElement)> = vec![];
+        let log_leaf_size = LOG_SLICE_NUMBER + 1;
+
+        let mut new_size = 0;
+        for i in 0..SLICE_NUMBER {
+            let element_1 = self.witness_rs_codeword_interleaved[oracle_indicator]
+                [pow_0 << log_leaf_size | i << 1 | 0];
+
+            let element_2 = self.witness_rs_codeword_interleaved[oracle_indicator]
+                [pow_0 << log_leaf_size | i << 1 | 1];
+
+            value.push((element_1, element_2));
+
+            if !self.visited_witness[oracle_indicator][pow_0 << log_leaf_size | i << 1 | 0] {
+                self.visited_witness[oracle_indicator][pow_0 << log_leaf_size | i << 1 | 0] = true;
+            }
+            if !self.visited_witness[oracle_indicator][pow_0 << log_leaf_size | i << 1 | 1] {
+                self.visited_witness[oracle_indicator][pow_0 << log_leaf_size | i << 1 | 1] = true;
+            }
+            new_size += std::mem::size_of::<FieldElement>();
+        }
+
+        let depth = self.log_current_witness_size_per_slice - 1;
+        let mut com_hhash: Vec<HashDigest> = Vec::with_capacity(depth);
+
+        // minus 1 since each leaf have 2 values (qual resi)
+        let mut pos = pow_0 + (1 << (self.log_current_witness_size_per_slice - 1));
+
+        let mut test_hash = self.witness_merkle[oracle_indicator][pos];
+        com_hhash[depth] = test_hash;
+
+        for i in 0..depth {
+            if !self.visited_init[oracle_indicator][pos ^ 1] {
+                new_size += std::mem::size_of::<HashDigest>();
+            }
+            self.visited_init[oracle_indicator][pos] = true;
+            self.visited_init[oracle_indicator][pos ^ 1] = true;
+
+            let data = if (pos & 1) == 1 {
+                [self.witness_merkle[oracle_indicator][pos ^ 1], test_hash]
+            } else {
+                [test_hash, self.witness_merkle[oracle_indicator][pos ^ 1]]
+            };
+            test_hash = my_hash::my_hash(data);
+
+            com_hhash[i] = self.witness_merkle[oracle_indicator][pos ^ 1];
+            pos /= 2;
+
+            assert_eq!(test_hash, self.witness_merkle[oracle_indicator][pos]);
+        }
+        assert!(pos == 1);
+        (value, com_hhash)
+    }
+
+    /// Request the merkle proof to lvl-th level oracle, at w^{pow}, will also return it's quad residue's proof.
+    /// returned value is unordered, meaning that one of them is the requested value and the other one is it's qual residue.
+    pub fn request_step_commit(
+        lvl: i64,
+        pow: i64,
+        new_size: i64,
+    ) -> (Vec<(FieldElement, FieldElement)>, Vec<HashDigest>) {
+        (vec![], vec![])
+    }
+
+    /// Given fold parameter r, return the root of the merkle tree of next level.
+    pub fn commit_phrase_step(r: FieldElement) {}
+
+    /// Return the final rs code since it is only constant size
+    pub fn commit_phase_final() -> FieldElement {
+        FieldElement::default()
+    }
+}
+
+// No longer needed, we got FRIContext
 pub struct VpdVerifier {
     cpd: FriCommitPhaseData,
     step: usize,
 }
 
+// SHould I put inside VPD?
 impl VpdVerifier {
     pub fn init() {}
 
@@ -114,33 +207,29 @@ impl VpdVerifier {
     }
 }
 
-/// Return the hhash array of commitments, randomness and final small polynomial (represented by rscode)
-pub fn commit_phase(log_length: usize) -> LdtCommitment {
-    // LOG_SLICE_NUMBER;
+// Return the hhash array of commitments, randomness and final small polynomial (represented by rscode)
+// pub fn commit_phase(log_length: usize) -> LdtCommitment {
+//     // LOG_SLICE_NUMBER;
 
-    let mut codeword_size = 1 << (log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
+//     let mut codeword_size = 1 << (log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
 
-    let mut randomness: Vec<FieldElement> =
-        Vec::with_capacity(log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
+//     let mut randomness: Vec<FieldElement> =
+//         Vec::with_capacity(log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
 
-    let mut ret: Vec<HashDigest> = Vec::with_capacity(log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
+//     let mut ret: Vec<HashDigest> = Vec::with_capacity(log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
 
-    let mut ptr = 0;
-    while codeword_size > (1 << RS_CODE_RATE) {
-        randomness[ptr] = prime_field::FieldElement::new_random();
-        ret[ptr] = commit_step(randomness[ptr]);
-        codeword_size /= 2;
-        ptr += 1;
-    }
+//     let mut ptr = 0;
+//     while codeword_size > (1 << RS_CODE_RATE) {
+//         randomness[ptr] = prime_field::FieldElement::new_random();
+//         ret[ptr] = commit_step(randomness[ptr]);
+//         codeword_size /= 2;
+//         ptr += 1;
+//     }
 
-    LdtCommitment {
-        commitment_hash: ret,
-        // final_rs_code: vpdVerifier::finish(),
-        randomness,
-        mx_depth: ptr,
-    }
-}
-
-pub fn commit_step(r: FieldElement) -> HashDigest {
-    HashDigest::new()
-}
+//     LdtCommitment {
+//         commitment_hash: ret,
+//         // final_rs_code: vpdVerifier::finish(),
+//         randomness,
+//         mx_depth: ptr,
+//     }
+// }
