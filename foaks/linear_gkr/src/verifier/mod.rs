092f138cec98250ac2be7925484b99aa031eea15
peter <thaiphuongn1@gmail.com>
2023-01-05 18:47:51 +0700

    verifier

--- foaks/linear_gkr/src/verifier/mod.rs
+++ foaks/linear_gkr/src/verifier/mod.rs
@@ -385,8 +385,6 @@ impl zk_verifier {
         for i in (self.aritmetic_circuit.total_depth - 1)..1 {
             let rho = FieldElement::new_random();
 
-            // p -> sumcheck_init(i, C.circuit[i].bit_length, C.circuit[i - 1].bit_length, C.circuit[i - 1].bit_length, alpha, beta, r_0, r_1, one_minus_r_0, one_minus_r_1);
-            // p -> sumcheck_phase1_init();
             zk_prover::sumcheck_init(
                 i,
                 self.aritmetic_circuit.circuit[i].bit_length,
@@ -401,6 +399,14 @@ impl zk_verifier {
                 one_minus_r_1,
             );
             zk_prover::sumcheck_phase1_init();
+
+            let previous_random = FieldElement::from_real(0);
+            //next level random
+            let r_u = FieldElement::new_random();
+            let r_v = FieldElement::new_random();
+
+            let direct_relay_value =
+                alpha * direct_relay(i, r_0, r_u) + beta * direct_relay(i, r_1, r_u);
         }
 
         todo!()
@@ -415,6 +421,8 @@ impl zk_verifier {
         }
         ret
     }
+
+    pub fn direct_relay() {}
 
     pub fn delete_self() {}
 }
