c7c1f01dfdd53b2af9f3247865af531799ec0f09
peter <thaiphuongn1@gmail.com>
2023-01-17 17:27:31 +0700

    update bash script

---
+++ foaks/linear_gkr/src/run.sh
@@ -0,0 +1,5 @@
+cargo run main.rs mat_16_circuit.txt mat_16_meta.txt LOG/mat_16.txt
+cargo run main.rs mat_32_circuit.txt mat_32_meta.txt LOG/mat_32.txt
+cargo run main.rs mat_64_circuit.txt mat_64_meta.txt LOG/mat_64.txt
+cargo run main.rs mat_128_circuit.txt mat_128_meta.txt LOG/mat_128.txt
+cargo run main.rs mat_256_circuit.txt mat_256_meta.txt LOG/mat_256.txt
