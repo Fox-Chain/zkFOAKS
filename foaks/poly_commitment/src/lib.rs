6033436ddaa50a052aba957e8281a0c1ca44c981
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-03-07 19:32:29 +0800

    Updat .gitignore

--- foaks/poly_commitment/src/lib.rs
+++ foaks/poly_commitment/src/lib.rs
@@ -9,129 +9,128 @@ use infrastructure::utility;
 
 #[derive(Default)]
 pub struct LdtCommitment {
-    pub commitment_hash: Vec<HashDigest>,
-    pub randomness: Vec<FieldElement>,
-    // final_rs_code: FieldElement,
-    pub mx_depth: usize,
-    // repeat_no: usize,
+  pub commitment_hash: Vec<HashDigest>,
+  pub randomness: Vec<FieldElement>,
+  // final_rs_code: FieldElement,
+  pub mx_depth: usize,
+  // repeat_no: usize,
 }
 
 #[derive(Default, Debug)]
 pub struct PolyCommitContext {
-    pub twiddle_factor: Vec<FieldElement>,
-    pub inv_twiddle_factor: Vec<FieldElement>,
-    pub twiddle_factor_size: usize,
-    pub inner_prod_evals: Vec<FieldElement>,
+  pub twiddle_factor: Vec<FieldElement>,
+  pub inv_twiddle_factor: Vec<FieldElement>,
+  pub twiddle_factor_size: usize,
+  pub inner_prod_evals: Vec<FieldElement>,
 
-    pub l_coef: Vec<FieldElement>,
-    pub l_coef_len: usize,
+  pub l_coef: Vec<FieldElement>,
+  pub l_coef_len: usize,
 
-    pub l_eval: Vec<FieldElement>,
-    pub l_eval_len: usize,
-    pub q_coef: Vec<FieldElement>,
-    pub q_coef_len: usize,
+  pub l_eval: Vec<FieldElement>,
+  pub l_eval_len: usize,
+  pub q_coef: Vec<FieldElement>,
+  pub q_coef_len: usize,
 
-    pub q_eval: Vec<FieldElement>,
-    pub q_eval_len: usize,
+  pub q_eval: Vec<FieldElement>,
+  pub q_eval_len: usize,
 
-    pub lq_coef: Vec<FieldElement>,
-    pub lq_eval: Vec<FieldElement>,
-    pub h_coef: Vec<FieldElement>,
-    pub h_eval: Vec<FieldElement>,
+  pub lq_coef: Vec<FieldElement>,
+  pub lq_eval: Vec<FieldElement>,
+  pub h_coef: Vec<FieldElement>,
+  pub h_eval: Vec<FieldElement>,
 
-    pub h_eval_arr: Vec<FieldElement>,
+  pub h_eval_arr: Vec<FieldElement>,
 
-    pub slice_size: usize,
-    pub slice_count: usize,
-    pub slice_real_ele_cnt: usize,
-    pub pre_prepare_executed: bool,
+  pub slice_size: usize,
+  pub slice_count: usize,
+  pub slice_real_ele_cnt: usize,
+  pub pre_prepare_executed: bool,
 }
 
 pub struct PolyCommitProver {
-    total_time: f64,
-    ctx: PolyCommitContext,
+  total_time: f64,
+  ctx: PolyCommitContext,
 }
 
 impl PolyCommitProver {
-    pub fn commit_private_array(
-        &mut self,
-        private_array: &[FieldElement],
-        log_array_length: usize,
-    ) -> HashDigest {
-        self.total_time = 0.;
+  pub fn commit_private_array(
+    &mut self,
+    private_array: &[FieldElement],
+    log_array_length: usize,
+  ) -> HashDigest {
+    self.total_time = 0.;
 
-        self.ctx.pre_prepare_executed = true;
+    self.ctx.pre_prepare_executed = true;
 
-        let slice_count = 1 << LOG_SLICE_NUMBER;
-        self.ctx.slice_count = slice_count;
+    let slice_count = 1 << LOG_SLICE_NUMBER;
+    self.ctx.slice_count = slice_count;
 
-        let slice_size = 1 << (log_array_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
-        self.ctx.slice_size = slice_size;
+    let slice_size = 1 << (log_array_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
+    self.ctx.slice_size = slice_size;
 
-        let slice_real_ele_cnt = slice_size >> RS_CODE_RATE;
-        self.ctx.slice_real_ele_cnt = slice_real_ele_cnt;
+    let slice_real_ele_cnt = slice_size >> RS_CODE_RATE;
+    self.ctx.slice_real_ele_cnt = slice_real_ele_cnt;
 
-        let l_eval_len = slice_count * slice_size;
-        self.ctx.l_eval_len = l_eval_len;
+    let l_eval_len = slice_count * slice_size;
+    self.ctx.l_eval_len = l_eval_len;
 
-        let l_eval = &mut self.ctx.l_eval;
-        l_eval.reserve(l_eval_len);
+    let l_eval = &mut self.ctx.l_eval;
+    l_eval.reserve(l_eval_len);
 
-        let mut tmp = Vec::<FieldElement>::with_capacity(slice_real_ele_cnt);
+    let mut tmp = Vec::<FieldElement>::with_capacity(slice_real_ele_cnt);
 
-        let order = slice_size * slice_count;
+    let order = slice_size * slice_count;
 
-        let now = time::Instant::now();
+    let now = time::Instant::now();
 
-        let mut scratch_pad = rs_polynomial::ScratchPad::from_order(order);
+    let mut scratch_pad = rs_polynomial::ScratchPad::from_order(order);
 
-        for i in 0..slice_count {
-            let mut all_zero = true;
-            let zero = FieldElement::zero();
+    for i in 0..slice_count {
+      let mut all_zero = true;
+      let zero = FieldElement::zero();
 
-            for j in 0..slice_real_ele_cnt {
-                if private_array[i * slice_real_ele_cnt + j] == zero {
-                    continue;
-                }
-                all_zero = false;
-                break;
-            }
-
-            if all_zero {
-                for j in 0..slice_size {
-                    l_eval[i * slice_size + j] = zero;
-                }
-            } else {
-                inverse_fast_fourier_transform(
-                    &mut scratch_pad,
-                    &private_array[i * slice_real_ele_cnt..],
-                    slice_real_ele_cnt,
-                    slice_real_ele_cnt,
-                    FieldElement::get_root_of_unity(utility::my_log(slice_real_ele_cnt).unwrap())
-                        .unwrap(),
-                    &mut tmp[..],
-                );
-
-                fast_fourier_transform(
-                    &mut scratch_pad.dst,
-                    &mut scratch_pad.twiddle_factor,
-                    &mut scratch_pad.twiddle_factor_size,
-                    &tmp[..],
-                    slice_real_ele_cnt,
-                    slice_size,
-                    FieldElement::get_root_of_unity(utility::my_log(slice_size).unwrap()).unwrap(),
-                    &mut l_eval[i * slice_size..],
-                )
-            }
+      for j in 0..slice_real_ele_cnt {
+        if private_array[i * slice_real_ele_cnt + j] == zero {
+          continue;
         }
+        all_zero = false;
+        break;
+      }
 
-        let elapsed_time = now.elapsed();
-        println!("FFT Prepare time: {} ms", elapsed_time.as_millis());
+      if all_zero {
+        for j in 0..slice_size {
+          l_eval[i * slice_size + j] = zero;
+        }
+      } else {
+        inverse_fast_fourier_transform(
+          &mut scratch_pad,
+          &private_array[i * slice_real_ele_cnt..],
+          slice_real_ele_cnt,
+          slice_real_ele_cnt,
+          FieldElement::get_root_of_unity(utility::my_log(slice_real_ele_cnt).unwrap()).unwrap(),
+          &mut tmp[..],
+        );
 
-        unimplemented!()
+        fast_fourier_transform(
+          &mut scratch_pad.dst,
+          &mut scratch_pad.twiddle_factor,
+          &mut scratch_pad.twiddle_factor_size,
+          &tmp[..],
+          slice_real_ele_cnt,
+          slice_size,
+          FieldElement::get_root_of_unity(utility::my_log(slice_size).unwrap()).unwrap(),
+          &mut l_eval[i * slice_size..],
+        )
+      }
     }
+
+    let elapsed_time = now.elapsed();
+    println!("FFT Prepare time: {} ms", elapsed_time.as_millis());
+
+    unimplemented!()
+  }
 }
 
 pub fn commit_phrase_step(r: FieldElement) -> HashDigest {
-    HashDigest::new()
+  HashDigest::new()
 }
