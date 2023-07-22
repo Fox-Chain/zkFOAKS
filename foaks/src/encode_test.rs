c3531fd03c64dc847e244efc59e4b8ebec4f814a
Juandac11 <burning3081@gmail.com>
2023-07-22 14:32:21 -0500

    Feat: convert encode_test c++ to rust

--- foaks/src/encode_test.rs
+++ foaks/src/encode_test.rs
@@ -1,130 +1,51 @@
-mod prime_field {
-  // Define the functions and types needed for the prime_field module
-  // (Implementation of the prime_field module is not provided here)
-  // ...
-}
+//import linear_code_encode modules
+use linear_code::linear_code_encode::*;
+//module to measure execution time.
+use std::time::Instant;
 
-struct Graph {
-  degree: usize,
-  neighbor: Vec<Vec<usize>>,
-  r_neighbor: Vec<Vec<usize>>,
-  weight: Vec<Vec<prime_field::FieldElement>>,
-  r_weight: Vec<Vec<prime_field::FieldElement>>,
-  l: usize,
-  r: usize,
-}
-
-impl Default for Graph {
-  fn default() -> Self {
-    Self {
-      degree: 0,
-      neighbor: Vec::new(),
-      r_neighbor: Vec::new(),
-      weight: Vec::new(),
-      r_weight: Vec::new(),
-      l: 0,
-      r: 0,
-    }
-  }
-}
-
-struct LinearCodeEncodeContext {
-  scratch: Vec<Vec<Vec<prime_field::FieldElement>>>,
-  c: Vec<Graph>,
-  d: Vec<Graph>,
-  encode_initialized: bool,
-}
-
-impl Default for LinearCodeEncodeContext {
-  fn default() -> Self {
-    let scratch = vec![vec![vec![prime_field::FieldElement::zero()]; 100]; 2];
-    let c = vec![Graph::default(); 100];
-    let d = vec![Graph::default(); 100];
-
-    Self {
-      scratch,
-      c,
-      d,
-      encode_initialized: false,
-    }
-  }
-}
-
-impl LinearCodeEncodeContext {
-  fn init() -> Self { Self::default() }
-
-  fn encode(
-    &mut self,
-    src: Vec<prime_field::FieldElement>,
-    n: usize,
-  ) -> (usize, Vec<prime_field::FieldElement>) {
-    // Implementation of the encode function goes here
-    // ...
-    // For demonstration purposes, we will return a fictitious value (n) for now
-    (n, src)
-  }
-
-  // Implementation of other functions goes here
-  // ...
-}
-
-fn generate_random_expander(l: usize, r: usize, d: usize) -> Graph {
-  // Implementation of the generate_random_expander function goes here
-  // ...
-  Graph::default() // For demonstration purposes, returning a default graph
-}
-
+//declaration of variables
 fn main() {
   prime_field::init();
+  let lgN: i32 = 20;
+  let N: usize = 1 << lgN;
+  let lgRate: i32 = 5;
+  let rs_rate: usize = 1 << lgRate;
 
-  let lgN = 20;
-  let N = 1 << lgN;
-  let lgRate = 5;
-  let rs_rate = 1 << lgRate;
-
-  let mut encoder = LinearCodeEncodeContext::init();
-
-  // infrastructure::expander_init(N); // Call this function if it is implemented
-
-  // infrastructure::init_scratch_pad(N * rs_rate * 2); // Call this function if it is implemented
-
-  let mut dst = vec![prime_field::FieldElement::new(0, 0); 2 * N];
-  let mut arr = vec![prime_field::FieldElement::new(0, 0); N];
-  let mut rscoef = vec![prime_field::FieldElement::new(0, 0); N];
-  let mut rsdst = vec![prime_field::FieldElement::new(0, 0); rs_rate * N];
-
+  //initialize expansion
+  expander_init(N);
+  //initialize an area of ​​memory used later in the code
+  init_scratch_pad(N * rs_rate * 2);
+  //Vector declaration and assignment
+  let mut arr: Vec<prime_field::field_element::FieldElement> = vec![prime_field::random(); N];
+  let mut dst: Vec<prime_field::field_element::FieldElement> =
+    vec![prime_field::field_element::FieldElement::default(); 2 * N];
+  let mut rscoef: Vec<prime_field::field_element::FieldElement> =
+    vec![prime_field::field_element::FieldElement::default(); N];
+  let mut rsdst: Vec<prime_field::field_element::FieldElement> =
+    vec![prime_field::field_element::FieldElement::default(); rs_rate * N];
+  //Random data generation
   for i in 0..N {
     arr[i] = prime_field::random();
   }
 
-  let t0 = std::time::Instant::now();
-  let (final_size, encoded_dst) = encoder.encode(arr, N);
-  dst[..final_size].copy_from_slice(&encoded_dst);
-  let t1 = std::time::Instant::now();
-  let duration = t1 - t0;
+  let t0 = Instant::now();
+  let final_size = encode(&arr, &mut dst, N);
+  let t1 = Instant::now();
+  let duration = t1.duration_since(t0).as_secs_f64();
 
-  let t0 = std::time::Instant::now();
-  inverse_fast_fourier_transform(
-    &mut arr,
-    N,
-    N,
-    prime_field::FieldElement::get_root_of_unity(lgN),
-    &mut rscoef,
-  );
+  // inverse fourier transform, measures time of each execution (high resolution)
+  let t0 = Instant::now();
+  inverse_fast_fourier_transform(&arr, N, N, prime_field::get_root_of_unity(lgN), &mut rscoef);
   fast_fourier_transform(
-    &mut rscoef,
+    &rscoef,
     N,
     rs_rate * N,
-    prime_field::FieldElement::get_root_of_unity(lgN + lgRate),
+    prime_field::get_root_of_unity(lgN + lgRate),
     &mut rsdst,
   );
-  let t1 = std::time::Instant::now();
-  let rs_duration = t1 - t0;
+  let t1 = Instant::now();
+  let rs_duration = t1.duration_since(t0).as_secs_f64();
 
-  println!(
-    "Encode time: {:.6} s, final size: {}",
-    duration.as_secs_f64(),
-    final_size
-  );
-  println!("RS Encode time: {:.6} s", rs_duration.as_secs_f64());
+  println!("Encode time {:.6}, final size {}", duration, final_size);
+  println!("RS Encode time {:.6}", rs_duration);
 }
