ef3d36653fd376b3a8401ef92cdfa5c60e0100bb
Juandac11 <burning3081@gmail.com>
2023-10-11 16:24:48 -0500

    Feat: improve and optimizing vifier on poly_com

--- foaks/poly_commitment/src/vpd/verifier.rs
+++ foaks/poly_commitment/src/vpd/verifier.rs
@@ -2,10 +2,10 @@ use std::mem;
 use std::time::Instant;
 
 use global::constants::FE_ZERO;
+use global::constants::*;
 use infrastructure::merkle_tree::create_tree;
 use infrastructure::my_hash::my_hash;
 use infrastructure::my_hash::{self, HashDigest};
-use global::constants::*;
 use prime_field::FieldElement;
 
 use crate::vpd::fri::FRIContext;
@@ -53,7 +53,6 @@ pub fn verify_merkle(
 }
 
 impl FRIContext {
-  /// Given fold parameter r, return the root of the merkle tree of next level.
   pub fn commit_phase_step(&mut self, r: FieldElement, slice_count: usize) -> HashDigest {
     let nxt_witness_size = (1 << self.log_current_witness_size_per_slice) / 2;
     if self.cpd.rs_codeword[self.current_step_no].is_empty() {
@@ -110,14 +109,11 @@ impl FRIContext {
         let c = i << log_leaf_size | (j << 1);
         let d = c | 1;
 
-        let rs_codeword_mapping = &mut self.cpd.rs_codeword_mapping;
-        let rs_codeword = &self.cpd.rs_codeword;
+        self.cpd.rs_codeword_mapping[self.current_step_no][a] = c;
+        self.cpd.rs_codeword_mapping[self.current_step_no][b] = c;
 
-        rs_codeword_mapping[self.current_step_no][a] = c;
-        rs_codeword_mapping[self.current_step_no][b] = c;
-
-        tmp[c] = rs_codeword[self.current_step_no][a];
-        tmp[d] = rs_codeword[self.current_step_no][b];
+        tmp[c] = self.cpd.rs_codeword[self.current_step_no][a];
+        tmp[d] = self.cpd.rs_codeword[self.current_step_no][b];
 
         assert!(a < nxt_witness_size * SLICE_NUMBER);
         assert!(b < nxt_witness_size * SLICE_NUMBER);
@@ -165,7 +161,6 @@ impl FRIContext {
     self.cpd.merkle[self.current_step_no - 1][1] // since we increment current_step_no up there
   }
 
-  /// Return the final rs code since it is only constant size
   pub fn commit_phase_final(&self) -> Vec<FieldElement> {
     self.cpd.rs_codeword[self.current_step_no - 1].clone()
   }
@@ -175,7 +170,6 @@ impl FRIContext {
 
     let log_current_witness_size_per_slice_cp = self.log_current_witness_size_per_slice;
     let mut codeword_size = 1 << (log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
-    // repeat until the codeword is constant
     let mut ret: Vec<HashDigest> = Vec::with_capacity(log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
     let mut randomness: Vec<FieldElement> =
       Vec::with_capacity(log_length + RS_CODE_RATE - LOG_SLICE_NUMBER);
