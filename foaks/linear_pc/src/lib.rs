b72e1acb536fa863207213fbd7e14e9c37c15776
Juandac11 <burning3081@gmail.com>
2023-10-11 16:40:05 -0500

    Feat: improve and optimizing lib on linear_pc

--- foaks/linear_pc/src/lib.rs
+++ foaks/linear_pc/src/lib.rs
@@ -1,11 +1,11 @@
 use std::{collections::HashMap, time::Instant};
 
+use global::constants::*;
 use infrastructure::{
   merkle_tree::{self, create_tree},
   my_hash::HashDigest,
   utility::my_log,
 };
-use global::constants::*;
 use linear_code::{
   linear_code_encode::LinearCodeEncodeContext,
   parameter::{CN, COLUMN_SIZE, DISTANCE_THRESHOLD, DN, TARGET_DISTANCE},
@@ -44,7 +44,7 @@ impl LinearPC {
     }
   }
   pub fn commit(&mut self, src: &[FieldElement]) -> Vec<HashDigest> {
-    //Todo: Refactor, delete self.codeword_size field
+    // Todo: Refactor, delete self.codeword_size field
     let n: usize = src.len();
 
     assert_eq!(n % COLUMN_SIZE, 0);
@@ -76,6 +76,7 @@ impl LinearPC {
       .collect();
 
     create_tree(&mut self.mt, &stash, true);
+
     self.mt.clone()
   }
 
@@ -413,9 +414,9 @@ impl LinearPC {
     (
       output_depth_output_size.0 + 1,
       *self
-          .gates_count
-          .get(&(output_depth_output_size.0 + 1))
-          .expect("Failed to retrieve gates_count value"),
+        .gates_count
+        .get(&(output_depth_output_size.0 + 1))
+        .expect("Failed to retrieve gates_count value"),
     )
   }
 
